d39e765483c0df862892590eea76021fc3e9a97e
Андрей Барышев <baryshev_2002@inbox.ru>
2024-01-04 17:55:03 +0600

    slider css modules

--- src/components/slider-section/SliderSection.tsx
+++ src/components/slider-section/SliderSection.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import './sliderSection.css'
+import styles from './sliderSection.module.css'
 import avatar from '../../assets/Group 5.svg'
 import dots from '../../assets/Group.svg'
 import element from '../../assets/element.svg'
@@ -9,26 +9,26 @@ import boldArrow from '../../assets/bold-arrow.svg'
 
 export const SliderSection: FC = () => {
     return (
-        <div className='flex align-center column'>
-            <div className='body margin-top flex column align-center'>
-                <img src={dots} className='dots' alt="" />
-                <h1 className='title'>What our customer are saying</h1>
-                <div className="divider"></div>
-                <div className="flex row align-center margin-top_50">
-                    <div className="profile flex row">
+        <div className={styles['parent']}>
+            <div className={styles['body']}>
+                <img src={dots} className={styles['dots']} alt="" />
+                <h1 className={styles['title']}>What our customer are saying</h1>
+                <div className={styles["divider"]}></div>
+                <div className={styles["profile-div"]}>
+                    <div className={styles["profile-div_child"]}>
                         <img src={avatar} alt="" />
-                        <div className="flex column margin-left_30 justify-center">
-                            <p className="bold margin_0">Edward Newgate</p>
-                            <p className="light margin_0">Dounder Circle</p>
+                        <div className={styles["profile-description"]}>
+                            <p className={styles["bold"]}>Edward Newgate</p>
+                            <p className={styles["light"]}>Dounder Circle</p>
                         </div>
                     </div>
-                    <p className="text width_420 margin-left_110 margin_0">“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”</p>
+                    <p className={styles["text"]}>“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”</p>
                 </div>
-                <img src={element} className='dots2' alt="" />
-                <div className='flex row justify-center'>
-                    <img src={lightArrow} className='slider' alt="" />
-                    <img src={slider} className='slider' alt="" />
-                    <img src={boldArrow} className='slider' alt="" />
+                <img src={element} className={styles['dots2']} alt="" />
+                <div className={styles['icons']}>
+                    <img src={lightArrow} className={styles['slider']} alt="" />
+                    <img src={slider} className={styles['slider']} alt="" />
+                    <img src={boldArrow} className={styles['slider']} alt="" />
                 </div>
             </div>
         </div>
