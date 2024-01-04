419464e46ecf2c77014ab41d6138286669381190
Андрей Барышев <baryshev_2002@inbox.ru>
2024-01-04 16:14:23 +0600

    servicecomponent css modules

--- src/components/services-component/BackElement.tsx
+++ src/components/services-component/BackElement.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react'
-import './serviceStyle.css'
+import styles from'./serviceStyle.module.css'
 import img from '../../assets/element2.svg'
 
 export const BackElement : FC = () => {
     return (
-    <img className='back-element' src={img} alt="image" />
+    <img className={styles['back-element']} src={img} alt="image" />
     );
 }
